ca3e8149d1d3fbdeedc242ceb85ad7351b65b3a0
Johannes Zarre <zarre.johannes@gmail.com>
2020-10-16 21:01:33 +0200

    started myscript.ts

--- myscript.js
+++ myscript.js
@@ -1,1 +1,51 @@
-console.log("JS Working");
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var locArray = [];
+var Locations = /** @class */ (function () {
+    function Locations(desc, zip, city, street, image) {
+        this.desc = desc;
+        this.zip = zip;
+        this.city = city;
+        this.street = street;
+        this.image = image;
+    }
+    Locations.prototype.display = function () {
+        return this.desc + " ZIP: " + this.zip + " City: " + this.city + "  Street: " + this.street + " " + this.image;
+    };
+    return Locations;
+}());
+var newLoc = new Locations("Daham", 1020, "Vienna", "Stuwerstrasse 47", "imglink");
+console.log(newLoc.display());
+var Restaurants = /** @class */ (function (_super) {
+    __extends(Restaurants, _super);
+    function Restaurants(desc, zip, city, streeet, image, link, tel) {
+        var _this = _super.call(this, desc, zip, city, streeet, image) || this;
+        _this.link = link;
+        _this.tel = tel;
+        return _this;
+    }
+    Restaurants.prototype.display1 = function () {
+        return this.display() + "link: " + this.link + " Tel: " + this.tel;
+    };
+    return Restaurants;
+}(Locations));
+var newRest = new Restaurants("The Stuwer", 1020, "Vienna", "Stuwerstrasse 50", "imglink", "restaurantwebsitelimk.com", "01-5239668");
+console.log(newRest.display1());
+var Events = /** @class */ (function (_super) {
+    __extends(Events, _super);
+    function Events() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Events;
+}(Locations));
--- myscript.ts
+++ myscript.ts
@@ -1,1 +1,51 @@
-console.log("JS Working");
+let locArray:Array<any> = [];
+
+class Locations{
+    desc:string;
+    zip:number;
+    city:string;
+    street:string;
+    image:string;
+
+constructor(desc,zip,city,street,image){
+    this.desc = desc;
+    this.zip = zip;
+    this.city = city;
+    this.street = street;
+    this.image = image;
+}
+
+display(){
+    return`${this.desc} ZIP: ${this.zip} City: ${this.city}  Street: ${this.street} ${this.image}`
+}
+
+}
+
+let newLoc = new Locations ("Daham",1020,"Vienna","Stuwerstrasse 47","imglink")
+console.log(newLoc.display());
+
+
+class Restaurants extends Locations{
+    link:string;
+    tel:any;
+constructor(desc,zip,city,streeet,image,link,tel){
+    super(desc,zip,city,streeet,image);
+    this.link = link;
+    this.tel = tel;
+}
+display1(){
+    return `${this.display()}link: ${this.link} Tel: ${this.tel}`
+
+}
+}
+
+let newRest = new Restaurants("The Stuwer",1020,"Vienna","Stuwerstrasse 50","imglink","restaurantwebsitelimk.com","01-5239668")
+ console.log(newRest.display1());
+
+class Events extends Locations {
+    link:string;
+    date:any;       //check again of displaying mb day/date etc//
+    entry:number;
+
+
+}
