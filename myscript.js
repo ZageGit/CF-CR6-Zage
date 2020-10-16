e8f0ffc76ff00236f59da56d2344d9d3f89c486b
Johannes Zarre <zarre.johannes@gmail.com>
2020-10-17 00:16:42 +0200

    add restaurants js innerHTML

--- myscript.js
+++ myscript.js
@@ -12,6 +12,8 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 var locArray = [];
+var restArray = [];
+var eventsArray = [];
 var Locations = /** @class */ (function () {
     function Locations(name, disc, zip, city, street, image) {
         this.name = name;
@@ -28,42 +30,47 @@ var Locations = /** @class */ (function () {
     return Locations;
 }());
 new Locations("Stephansdom", "Stephansdom halt", 1010, "Vienna", "Stephansplatz 3", "img/stephansdom.jpg");
-new Locations("Schönbrunn", "Schönbrunn,schön wie immer", 1130, "Vienna", "Schönbrunner Schloßstraße 50 ", "img/hundertwasser.jpg");
-new Locations("Schönbrunn", "Schönbrunn,schön wie immer", 1130, "Vienna", "Schönbrunner Schloßstraße 50 ", "img/riesenrad.jpg");
-new Locations("Schönbrunn", "Schönbrunn,schön wie immer", 1130, "Vienna", "Schönbrunner Schloßstraße 50 ", "img/schonbrunn.jpg");
+new Locations("Schönbrunn", "Schönbrunn wie immer", 1130, "Vienna", "Schönbrunner Schloßstraße 50 ", "img/schonbrunn.jpg");
+new Locations("Riesenrad", "Prater bam Oida", 1020, "Vienna", "Prater 1", "img/riesenrad.jpg");
+new Locations("Hundertwasserhaus", "Echt oag", 1030, "Vienna", "Kegelgasse 36-38", "img/hundertwasser.jpg");
 for (var i in locArray) {
     document.getElementById("locations").innerHTML += locArray[i].renderLoc();
 }
 var Restaurants = /** @class */ (function (_super) {
     __extends(Restaurants, _super);
-    function Restaurants(name, disc, zip, city, street, image, link, tel) {
+    function Restaurants(name, disc, zip, city, street, image, link, tel, cousine) {
         var _this = _super.call(this, name, disc, zip, city, street, image) || this;
         _this.link = link;
         _this.tel = tel;
+        _this.cousine = cousine;
+        restArray.push(_this);
         return _this;
     }
-    Restaurants.prototype.display1 = function () {
-        return this.renderLoc() + "link: " + this.link + " Tel: " + this.tel;
+    Restaurants.prototype.renderRest = function () {
+        return "<div class=\"col-sm-12 col-md-6 col-lg-3 mb-4\">\n    <img class=\"card-img-top\" src=\"" + this.image + "\"  alt=\"\">\n    <div class=\"card-body center-block border\">\n      <h5 class=\"card-title\">" + this.name + "</h5>\n      <p class=\"card-text\">" + this.disc + "</p>\n      <p class=\"card-text\">" + this.zip + ", " + this.city + ", " + this.street + "</p>\n      <p class=\"card-text\"><a href=\"" + this.link + "\"></a></p>\n      <p class=\"card-text\">" + this.tel + "</p>\n      <p class=\"card-text\">" + this.cousine + "</p>\n    </div>\n    <div class=\"card-footer d-flex justify-content-center border\">\n      <a href=\"#\" class=\"btn btn-primary\">Go somewhere</a>\n    </div>\n    </div>\n    ";
     };
     return Restaurants;
 }(Locations));
-var newRest = new Restaurants("The Stuwer", "beste schnitzel", 1020, "Vienna", "Stuwerstrasse 50", "imglink", "restaurantwebsitelimk.com", "01-5239668");
-console.log(newRest.display1());
-var Events = /** @class */ (function (_super) {
-    __extends(Events, _super);
-    function Events(name, disc, zip, city, street, image, link, date, entry) {
-        var _this = _super.call(this, name, disc, zip, city, street, image) || this;
-        _this.date = new Date(); //check again of displaying mb day/date etc//
-        _this.link = link;
-        _this.date = date;
-        _this.entry = entry;
-        return _this;
-    }
-    Events.prototype.display2 = function () {
-        return this.renderLoc() + " link: " + this.link + " date: " + this.date + " entry: " + this.entry + " \u20AC";
-    };
-    return Events;
-}(Locations));
-var newEvent = new Events("zarrah live Rap", "Aus ohhr", 1030, "Budapest", "king streeet 98", "imamgeelink", "websitelinkevent.com", "25.11.2020", 54);
-console.log(newEvent.display2());
+new Restaurants("Des Stuweer", "urig beisln", 1010, "Vienna", "Stephansplatz 3", "img/rest1.jpg", "www.dasstuweer.at", "01/52689", "Wienerisch");
+new Restaurants("Hansy", "schlechte Küche dafür unfreundliche Bedienung", 1020, "Vienna", "Heinestraße 42", "img/rest2.jpg", "www.hansy.at", "01/backhendl", "Alt-Wienerisch");
+new Restaurants("BBQ-XXL", "Große Schnitzel Große Bier", 1110, "Vienna", "Orge Street 12", "img/rest3.jpg", "www.bbqxxl.at", "01/55XXL", "Nix spezielles nur groß");
+new Restaurants("Da Heirige", "Das beste von deen vier", 1220, "Vienna", "Heirignhigl 12", "img/rest4.jpg", "www.wasistinternet.at", "01/ichhabnochfax", "Breddljausn");
+for (var i in locArray) {
+    document.getElementById("restaurants").innerHTML += restArray[i].renderRest();
+}
+// class Events extends Locations {
+//     link:string;
+//     date=new Date();       //check again of displaying mb day/date etc//
+//     entry:number;
+// constructor(name,disc,zip,city,street,image,link,date,entry){
+//     super(name,disc,zip,city,street,image);
+//     this.link=link;
+//     this.date=date;
+//     this.entry=entry;
+// }
+// display2(){
+//     return `${this.renderLoc()} link: ${this.link} date: ${this.date} entry: ${this.entry} €`
+// }
+// }
+// let newEvent = new Events ("zarrah live Rap","Aus ohhr",1030,"Budapest","king streeet 98","imamgeelink","websitelinkevent.com", "25.11.2020",54)
 console.table(locArray);
--- myscript.ts
+++ myscript.ts
@@ -1,4 +1,6 @@
 let locArray:Array<any> = [];
+let restArray:Array<any> = [];
+let eventsArray:Array<any> = [];
 
 class Locations{
     name:string;
@@ -7,6 +9,7 @@ class Locations{
     city:string;
     street:string;
     image:string;
+    
 
 constructor(name,disc,zip,city,street,image){
     this.name = name;
@@ -16,6 +19,7 @@ constructor(name,disc,zip,city,street,image){
     this.street = street;
     this.image = image;
     locArray.push(this);
+    
 }
 
 renderLoc(){
@@ -36,9 +40,9 @@ renderLoc(){
 }
 
 new Locations ("Stephansdom","Stephansdom halt",1010,"Vienna","Stephansplatz 3","img/stephansdom.jpg")
-new Locations ("Schönbrunn","Schönbrunn,schön wie immer",1130,"Vienna","Schönbrunner Schloßstraße 50 ","img/hundertwasser.jpg")
-new Locations ("Schönbrunn","Schönbrunn,schön wie immer",1130,"Vienna","Schönbrunner Schloßstraße 50 ","img/riesenrad.jpg")
-new Locations ("Schönbrunn","Schönbrunn,schön wie immer",1130,"Vienna","Schönbrunner Schloßstraße 50 ","img/schonbrunn.jpg")
+new Locations ("Schönbrunn","Schönbrunn wie immer",1130,"Vienna","Schönbrunner Schloßstraße 50 ","img/schonbrunn.jpg")
+new Locations ("Riesenrad","Prater bam Oida",1020,"Vienna","Prater 1","img/riesenrad.jpg")
+new Locations ("Hundertwasserhaus","Echt oag",1030,"Vienna","Kegelgasse 36-38","img/hundertwasser.jpg")
 
 for (let i in locArray){
     document.getElementById("locations").innerHTML+=locArray[i].renderLoc();
@@ -49,36 +53,65 @@ for (let i in locArray){
 class Restaurants extends Locations{
     link:string;
     tel:any;
-constructor(name,disc,zip,city,street,image,link,tel){
+    cousine:string;
+constructor(name,disc,zip,city,street,image,link,tel,cousine){
     super(name,disc,zip,city,street,image);
     this.link = link;
     this.tel = tel;
+    this.cousine=cousine;
+    restArray.push(this);
+
 }
-display1(){
-    return `${this.renderLoc()}link: ${this.link} Tel: ${this.tel}`
+renderRest(){
+    return `<div class="col-sm-12 col-md-6 col-lg-3 mb-4">
+    <img class="card-img-top" src="${this.image}"  alt="">
+    <div class="card-body center-block border">
+      <h5 class="card-title">${this.name}</h5>
+      <p class="card-text">${this.disc}</p>
+      <p class="card-text">${this.zip}, ${this.city}, ${this.street}</p>
+      <p class="card-text"><a href="${this.link}"></a></p>
+      <p class="card-text">${this.tel}</p>
+      <p class="card-text">${this.cousine}</p>
+    </div>
+    <div class="card-footer d-flex justify-content-center border">
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    </div>
+    `
 
 }
 }
 
-let newRest = new Restaurants("The Stuwer","beste schnitzel",1020,"Vienna","Stuwerstrasse 50","imglink","restaurantwebsitelimk.com","01-5239668")
- console.log(newRest.display1());
+new Restaurants ("Des Stuweer","urig beisln",1010,"Vienna","Stephansplatz 3","img/rest1.jpg","www.dasstuweer.at","01/52689","Wienerisch")
+new Restaurants ("Hansy","schlechte Küche dafür unfreundliche Bedienung",1020,"Vienna","Heinestraße 42","img/rest2.jpg","www.hansy.at","01/backhendl","Alt-Wienerisch")
+new Restaurants ("BBQ-XXL","Große Schnitzel Große Bier",1110,"Vienna","Orge Street 12","img/rest3.jpg","www.bbqxxl.at","01/55XXL","Nix spezielles nur groß")
+new Restaurants ("Da Heirige","Das beste von deen vier",1220,"Vienna","Heirignhigl 12","img/rest4.jpg","www.wasistinternet.at","01/ichhabnochfax","Breddljausn")
 
-class Events extends Locations {
-    link:string;
-    date=new Date();       //check again of displaying mb day/date etc//
-    entry:number;
-constructor(name,disc,zip,city,street,image,link,date,entry){
-    super(name,disc,zip,city,street,image);
-    this.link=link;
-    this.date=date;
-    this.entry=entry;
-}
-display2(){
-    return `${this.renderLoc()} link: ${this.link} date: ${this.date} entry: ${this.entry} €`
+for (let i in locArray){
+    document.getElementById("restaurants").innerHTML+=restArray[i].renderRest();
 }
 
-}
 
-let newEvent = new Events ("zarrah live Rap","Aus ohhr",1030,"Budapest","king streeet 98","imamgeelink","websitelinkevent.com", "25.11.2020",54)
-console.log(newEvent.display2());
+
+
+
+ 
+
+// class Events extends Locations {
+//     link:string;
+//     date=new Date();       //check again of displaying mb day/date etc//
+//     entry:number;
+// constructor(name,disc,zip,city,street,image,link,date,entry){
+//     super(name,disc,zip,city,street,image);
+//     this.link=link;
+//     this.date=date;
+//     this.entry=entry;
+// }
+// display2(){
+//     return `${this.renderLoc()} link: ${this.link} date: ${this.date} entry: ${this.entry} €`
+// }
+
+// }
+
+// let newEvent = new Events ("zarrah live Rap","Aus ohhr",1030,"Budapest","king streeet 98","imamgeelink","websitelinkevent.com", "25.11.2020",54)
 console.table(locArray);
